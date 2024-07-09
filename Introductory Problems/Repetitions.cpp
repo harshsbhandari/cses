1d32ada8168d277beac7b1a2712100d17aff454c
harshsbhandari <harshsbhandari.com>
2024-07-09 09:01:33 +0530

    Repetitions

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define int long long 
+#define float double
+#define endl '\n'
+
+void yo(){
+    string s;
+    cin>>s;
+    int n = s.length(), ans = INT_MIN;
+
+    if(n == 1){
+        cout<<"1";
+        return ;
+    }
+
+    char x = s[0];
+    int count = 1;
+
+    for(int i = 1; i < n; i++){
+        if(s[i] == x)
+            count++;
+        else{
+            x = s[i];
+            count = 1;
+        }
+
+        ans = max(ans, count);
+    }
+    
+    cout<<ans;
+}
+
+int32_t main(){
+    fast;
+    
+    // int tt;
+    // cin>>tt;
+    
+    // while(tt--)
+        yo();
+	
+    return 0;
+}
